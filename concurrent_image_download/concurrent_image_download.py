c8b06147e5c0caafbd824751a1bed6b87e846471
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-03-29 14:55:36 +0100

    Add threading solution.

---
+++ concurrent_image_download/concurrent_image_download.py
@@ -0,0 +1,43 @@
+import threading
+import urllib.request
+import time
+
+
+NUMBER_OF_THREADS = 10
+
+
+def download_image(image_path, file_name):
+    print(f'Downloading image from: {image_path}')
+    urllib.request.urlretrieve(image_path, file_name)
+    print('Completed Download')
+
+
+def execute_thread(thread_number):
+    image_name = f'temp/image-{thread_number}.jpg'
+    download_image('https://picsum.photos/400/200', image_name)
+
+
+def main():
+    start = time.time()
+    # create an array to store references to our threads
+    threads = []
+
+    # create the threads first and append them to the array to have their references
+    for i in range(NUMBER_OF_THREADS):
+        thread = threading.Thread(target=execute_thread, kwargs={'thread_number': i})
+        threads.append(thread)
+        # This starts each thread
+        thread.start()
+
+    for thread in threads:
+        # calling join() on the thread blocks until it completes. Doing this in a loop makes sure that we stop execution
+        # of the further part of the code until all threads have completed their execution first.
+        thread.join()
+
+    stop = time.time()
+    total_time = stop - start
+    print(f'Total execution time: {total_time}')
+
+
+if __name__ == '__main__':
+    main()
